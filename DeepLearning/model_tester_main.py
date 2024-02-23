d0eb420657f2dc0210b3ebb18823e018274e7d49
iAmGiG <gigachris1@hotmail.com>
2024-02-23 13:02:49 -0500

    ok, so making progress. looks like the lead is happy with the results. will make a new readme for the next steps and the next branch.

--- DeepLearning/model_tester_main.py
+++ DeepLearning/model_tester_main.py
@@ -12,7 +12,7 @@ testX = testX / 255.0  # Normalize
 testY = to_categorical(testY)
 
 # make this line abstract based on flags.
-model_path = './DeepLearning/SavedStandardModels/mnist_model_DWT.h5'
+model_path = './DeepLearning/SavedStandardModels/mnist_model_02-20_17-44.h5'
 
 # Load your model
 model = load_model(model_path)
@@ -38,3 +38,39 @@ zero_weights = np.sum([np.sum(w == 0) for w in weights])
 total_weights = np.sum([w.size for w in weights])
 sparsity = zero_weights / total_weights
 print(f"Sparsity: {sparsity*100:.2f}%")
+
+
+"""
+model 1 and trained with mnist.
+model 2 but quantized with dwt.
+now what about the paramater size?
+what is the parma size?
+with the mnist,
+now have a threashold for the quantization, give this,
+lets quantize more on the param sizes,
+repeat thsi and increase the streangh of the reduciton
+then find the point where the therashold pushes 
+
+where does the reduction threashold push the accuracy off the cliff.
+evaluate the size and contnue to explore the 
+
+param size vs the performance, 
+find any critical points of % reduction.
+
+what is the performance chagne after x% change?
+accuracy, ...ect.
+
+now we can measure the performnce of proof of concept, 
+the take on the BERT, ~~LLaMA~~, ResNet-pretrained
+then do this on the text and image.
+
+how much size reduction can we do before we end up reducing to much to not enough?
+DWT matrix reduction, and reducing more, reconstruct back, and reconstruct the size.
+then performance test.
+
+relation between performnace and size reduction.
+then far away is the adaptive learning process.'
+put this in a chart, make the 
+PURPOSE: and then we can improve the vision then reduce the size for mobile/edge devices.
+
+"""
