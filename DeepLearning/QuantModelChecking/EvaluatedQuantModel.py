32c589082c562686111b688de9eb502e5954304a
iAmGiG <gigachris1@hotmail.com>
2024-03-04 21:09:30 -0500

    added code linting

--- DeepLearning/QuantModelChecking/EvaluatedQuantModel.py
+++ DeepLearning/QuantModelChecking/EvaluatedQuantModel.py
@@ -1,21 +1,56 @@
-import numpy as np
-import tensorflow as tf
 from tensorflow.keras.datasets import mnist
+import tensorflow as tf
+import numpy as np
+from absl import app
+from absl import flags
+import os
+import re
+from datetime import datetime
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string('model_dir', './SavedTFliteModels',
+                    'Directory where TFLite models are saved.')
+
+
+def find_most_recent_model(directory):
+    # Regex to extract datetime and possibly threshold from filenames
+    pattern = re.compile(r'.*_(\d{8}T\d{6})_.*\.tflite$')
+    models = [f for f in os.listdir(directory) if pattern.match(f)]
+    if not models:
+        return None
+    latest_model = max(models, key=lambda x: datetime.strptime(
+        pattern.match(x).group(1), '%Y%m%dT%H%M%S'))
+    return os.path.join(directory, latest_model)
 
 # Load MNIST test dataset
-(testX, testY), (_, _) = mnist.load_data()
-testX = testX.astype('float32') / 255.0
-testX = np.expand_dims(testX, -1)  # Make sure to match the model's input shape
-testY = tf.keras.utils.to_categorical(testY)
+
+
+def load_test_dataset():
+    (testX, testY), (_, _) = mnist.load_data()
+    testX = testX.astype('float32') / 255.0
+    # Make sure to match the model's input shape
+    testX = np.expand_dims(testX, -1)
+    testY = tf.keras.utils.to_categorical(testY)
+    return testX, testY
 
 # Load the TFLite model and allocate tensors
-interpreter = tf.lite.Interpreter(
-    model_path='./DeepLearning/SavedStandardModels/mnist_model_nn_quant.tflite')
-interpreter.allocate_tensors()
+
+
+def make_interpreter():
+    model_path = find_most_recent_model(FLAGS.model_dir)
+    if model_path is None:
+        raise FileNotFoundError(
+            "No TFLite model found in the specified directory.")
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    return interpreter
 
 # Get input and output tensors
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+
+
+def get_i_o_details(interpreter):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
 
 # Define a function to evaluate accuracy
 
@@ -23,35 +58,46 @@ output_details = interpreter.get_output_details()
 def evaluate_model(interpreter, testX, testY):
     input_index = interpreter.get_input_details()[0]['index']
     output_index = interpreter.get_output_details()[0]['index']
-    
+
     prediction_digits = []
     for test_image in testX:
         # Ensure test_image is float32
         test_image = test_image.astype(np.float32)
-        
+
         # Make sure test_image is of shape [28, 28] (or [28, 28, 1] and then squeeze if needed)
         if test_image.ndim == 4:  # Assuming the shape is [1, 28, 28, 1]
             test_image = np.squeeze(test_image, axis=0)  # Now [28, 28, 1]
         if test_image.ndim == 3 and test_image.shape[-1] == 1:
-            test_image = np.squeeze(test_image, axis=-1)  # Squeeze channel dimension if it's 1
-        
+            # Squeeze channel dimension if it's 1
+            test_image = np.squeeze(test_image, axis=-1)
+
         # Add batch dimension
         test_image = np.expand_dims(test_image, axis=0)
-        
+
         interpreter.set_tensor(input_index, test_image)
         interpreter.invoke()
-        
+
         output_data = interpreter.get_tensor(output_index)
         digit = np.argmax(output_data[0])
         prediction_digits.append(digit)
 
     # Compute accuracy
-    accurate_count = sum(1 for i in range(len(prediction_digits)) if prediction_digits[i] == np.argmax(testY[i]))
+    accurate_count = sum(1 for i in range(len(prediction_digits))
+                         if prediction_digits[i] == np.argmax(testY[i]))
     accuracy = accurate_count / len(prediction_digits)
     return accuracy
 
 
+def main(argv):
+    if len(argv) > 1:
+        raise app.UsageError('Too many command-line arguments.')
+    # Evaluate the model
+    testX, testY = load_test_dataset()
+    interpreter = make_interpreter()
+    get_i_o_details(interpreter=interpreter)
+    accuracy = evaluate_model(interpreter, testX, testY)
+    print(f'Quantized model accuracy: {accuracy*100:.2f}%')
 
-# Evaluate the model
-accuracy = evaluate_model(interpreter, testX, testY)
-print(f'Quantized model accuracy: {accuracy*100:.2f}%')
+
+if __name__ == '__main__':
+    app.run(main)
--- DeepLearning/QuantModelChecking/TensorFlowLite.py
+++ DeepLearning/QuantModelChecking/TensorFlowLite.py
@@ -1,7 +1,13 @@
 import tensorflow as tf
+from absl import app
+from absl import flags
+
+FLAGS = flags.FLAGS
+
 
 # Load the existing TensorFlow model
-model = tf.keras.models.load_model('./DeepLearning/SavedStandardModels/mnist_model_02-20_17-53.h5')
+model = tf.keras.models.load_model(
+    './DeepLearning/SavedStandardModels/mnist_model_02-20_17-53.h5')
 
 # Set up the converter for post-training quantization
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
@@ -13,3 +19,12 @@ tflite_quant_model = converter.convert()
 # Save the quantized model
 with open('./DeepLearning/SavedStandardModels/mnist_model_quant.tflite', 'wb') as f:
     f.write(tflite_quant_model)
+
+
+def main(argv):
+    if len(argv) > 1:
+        raise app.UsageError('Too many command-line arguments.')
+
+
+if __name__ == '__main__':
+    app.run(main)
