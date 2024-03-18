59f3f8191ef055e7468defc5ac41f3ce08b4eda1
iAmGiG <gigachris1@hotmail.com>
2024-03-18 00:33:45 -0400

    more testing still having envrionment issues.

--- DeepLearning/model_tester_main.py
+++ DeepLearning/model_tester_main.py
@@ -1,7 +1,6 @@
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-from tensorflow import keras
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import load_model
