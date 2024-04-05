00dc6b5e00f687e3e75bc7b46bc0eea2e53265fc
iAmGiG <gigachris1@hotmail.com>
2024-04-05 11:20:51 -0400

    added some TODOs

--- DeepLearning/DWT_Pruning.py
+++ DeepLearning/DWT_Pruning.py
@@ -7,6 +7,21 @@ import pywt
 import uuid
 import json
 
+"""
+TODO: show the linear relationship of pruning and threshold dropping.
+so when we prune how much, 
+when we modify the percent
+
+we want to see about pruning a selection by percentage that isn't random.
+at what point do we see a large accuracy drop?
+how much can we go by this method.
+
+need to understand HOW this pruning is working, 
+are the weights begin adjusted, 
+the weights are staying?
+TODO: HOW ARE THE VALUES BEIGN PRUNNED
+"""
+
 # Define Flags.
 FLAGS = flags.FLAGS
 '''
@@ -50,9 +65,8 @@ flags.DEFINE_string('model_path', None,
 flags.DEFINE_enum('wavelet', 'haar', ['haar', 'db1', 'db2', 'coif1', 'bior1.3', 'rbio1.3', 'sym2', 'mexh', 'morl'],
                   'Type of wavelet to use for DWT.')
 flags.DEFINE_integer('level', '1', 'Decomposition level for the DWT.')
-flags.DEFINE_float('threshold', '0.85',
+flags.DEFINE_float('threshold', '0.085',
                    'Threshold for thresholding the wavelet coefficients obtained from the DWT.')
-
 flags.DEFINE_string('quant_level', 'binary',
                     'Level of quantization (binary, ternary, etc.)')
 flags.DEFINE_float('prun_percent', 50, 'Percentage of weights to quantize')
--- DeepLearning/Shallow_CNN_Generator.py
+++ DeepLearning/Shallow_CNN_Generator.py
@@ -1,12 +1,25 @@
-import os
-import datetime as datetime
-import tensorflow as tf
+from absl import app, flags
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.utils import to_categorical
+import os
+import datetime as datetime
+import tensorflow as tf
 import uuid
 import json
+
+#TODO log
+"""
+TODO: get model changes log.json incoporated into shallow cnn gen
+TODO: implement absl flags ~~~See Train Model~~~
+TODO: 
+"""
+
+FLAGS = flags.FLAGS
+flags.DEFINE_integer('batch_size', 32, 'Batch size for training.')
+flags.DEFINE_integer(
+    'epochs', '10', 'Number of epochs or iterations over the entire training dataset.')
 
 save_dir = './DeepLearning/SavedStandardModels'
 os.makedirs(save_dir, exist_ok=True)
@@ -28,7 +41,7 @@ def save_model(model, original_model_path, guid):
     # Determine the directory of the original model
     directory = os.path.dirname(original_model_path)
     # Create a new directory name with the GUID
-    new_directory_name = f"{directory}/optimized_{guid}"
+    new_directory_name = f"{directory}/shallow_cnn_{guid}"
     os.makedirs(new_directory_name, exist_ok=True)
     # Save the model in the new directory
     model_save_path = f"{new_directory_name}/model.h5"
@@ -37,6 +50,9 @@ def save_model(model, original_model_path, guid):
 
 
 def load_dataset():
+    """
+    Loads the mnist dataset
+    """
     # Load dataset
     (trainX, trainY), (testX, testY) = mnist.load_data()
     # Normalize images
@@ -49,6 +65,10 @@ def load_dataset():
 
 
 def define_model():
+    """
+    a shallow cnn model
+    TODO: fix "if needed" the model scope issue.
+    """
     model = Sequential([
         Flatten(input_shape=(28, 28)),
         Dense(128, activation='relu'),
@@ -60,6 +80,9 @@ def define_model():
 
 
 def train_model():
+    """
+    TODO: might rename things?
+    """
     trainX, trainY, testX, testY = load_dataset()
     model = define_model()
     model.fit(trainX, trainY, epochs=10, batch_size=32,
@@ -68,9 +91,20 @@ def train_model():
     return model
 
 
-if __name__ == "__main__":
-    model_filename = f"mnist_model_{date_time}.h5"
-    full_path = os.path.join(save_dir, model_filename)
-    model = train_model()
-    model.summary()
-    print(f"Model saved as {model_filename}")
+def main(argv):
+    """
+    does a thing and runs the show
+    """
+    # model_filename = f"mnist_model_{date_time}.h5"
+    # full_path = os.path.join(save_dir, model_filename)
+    # model = train_model()
+    # model.summary()
+    # print(f"Model saved as {model_filename}")
+    # Load and preprocess the MNIST dataset
+    batch_size = FLAGS.batch_size
+    
+    trainX, trainY, testX, testY = load_dataset()
+
+
+if __name__ == '__main__':
+    app.run(main)
