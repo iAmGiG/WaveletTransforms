8068313b7ea1617e213a08209eaeb72eb8abad65
WormsCanned <gigachris1@hotmail.com>
2024-07-03 16:11:16 -0400

    Working towards the eval phase.

--- ResNet/data_prep.py
+++ ResNet/data_prep.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
+import torch
 
 class CustomImageDataset(Dataset):
     def __init__(self, image_dir, transform=None):
@@ -19,16 +20,22 @@ class CustomImageDataset(Dataset):
             image = self.transform(image)
         return image
 
-def prepare_test_dataloader(test_dir, batch_size=32):
+def prepare_and_save_data(test_dir, save_dir, batch_size=32):
     transform = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
+    
     test_dataset = CustomImageDataset(test_dir, transform=transform)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
-    return test_loader
 
-# Prepare DataLoader for the test images
-test_loader = prepare_test_dataloader('imagenet-1k/data/test_images')
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    for i, images in enumerate(test_loader):
+        torch.save(images, os.path.join(save_dir, f'batch_{i}.pt'))
+
+# Run this only once to preprocess and save the data
+prepare_and_save_data('imagenet-1k/data/test_images', 'preprocessed_test_data')
